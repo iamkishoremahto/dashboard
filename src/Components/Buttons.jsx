f27d51e9ecf76dc0782b09bbba41765dd06bd72b
iamkishoremahto <iamkishoremahto@gmail.com>
2024-08-02 18:21:38 +0530

    cursor changed

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -28,7 +28,7 @@ function ThemeModeSwitch() {
  
   
   return (
-    <label htmlFor="switch" onClick={ updateClass }  className={` relative ${checkbox?'bg-indigo-500':'bg-neutral-600'}  transition-all h-[30px] w-[50px] rounded-[35px] `}>
+    <label htmlFor="switch" onClick={ updateClass }  className={` relative cursor-pointer ${checkbox?'bg-indigo-500':'bg-neutral-600'}  transition-all h-[30px] w-[50px] rounded-[35px] `}>
       <input type="checkbox" onChange={() =>{setCheckbox((state) => !state)}} id="switch" ref={checkBoxField} className="w-0 h-0 opacity-0" />
       <span className={`slider absolute top-[50%]  transition-all ${checkbox?'left-[40%]':'left-[0%]'}   translate-y-[-50%] mx-1  rounded-full  min-h-[20px] min-w-[20px] bg-white`}></span>
     </label>
