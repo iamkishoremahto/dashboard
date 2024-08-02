6942dc8a36befa5f84e71f49b2629f7c51e3b627
Kishore Kumar Mahto <iamkishoremahto@gmail.com>
2024-08-03 00:25:22 +0530

    working on navbar

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,9 +1,48 @@
 
 import { useRef, useState, useEffect } from "react"
+import { NavLink } from 'react-router-dom'
+import { IoMdLogOut } from "react-icons/io";
 
 function Button1({ icon , onClick}) {
   return (
     <button className="  text-[25px] p-3 rounded-full bg-neutral-100 text-neutral-500 dark:text-white dark:bg-neutral-700 hover:bg-neutral-200 dark:hover:bg-neutral-600 transition-all" onClick={onClick && ''}>{ icon }</button>
+  )
+}
+
+function LogoutBtn(){
+  return <>
+  <button className=" text-gray-500 dark:text-indigo-100  transition-all hover:text-indigo-500 py-3 text-[35px] flex flex-items justify-center w-full">
+  <IoMdLogOut />
+  </button>
+  </>
+}
+
+function MenuButton({ href, hrefText }){
+  return (
+    <>
+      <NavLink to={ href } className={({ isActive }) =>(
+      ` transition-all text-[35px] relative py-2 ${isActive ? 'text-indigo-500': ' dark:text-indigo-100 text-gray-500'} hover:text-indigo-500 w-full flex items-center justify-center 
+      
+      
+      before:context[]
+      before:absolute
+      lg:before:h-full
+      lg:before:w-[5px]
+
+      before:left-0
+      before:transition-all
+      before:bg-indigo-500
+      before:opacity-0
+      hover:before:opacity-100
+      ${isActive ? 'before:opacity-100':'before:opacity-0'}
+
+      
+      `)
+      }>
+      { hrefText }
+      </NavLink>
+    
+    </>
   )
 }
 
@@ -35,4 +74,4 @@ function ThemeModeSwitch() {
   )
 }
 
-export { Button1,ThemeModeSwitch } 
+export { Button1,ThemeModeSwitch, MenuButton, LogoutBtn } 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
+import { MenuButton, LogoutBtn } from "./Buttons";
+import { IoMdHome } from "react-icons/io";
+import { BsFileEarmarkBarGraph } from "react-icons/bs";
+import { BiTask } from "react-icons/bi";
+import { CiWallet } from "react-icons/ci";
+import { BsBagCheck } from "react-icons/bs";
 
 
 export default function Navbar() {
   return (
-    <div className=" lg:relative absolute bottom-0 dark:text-white bg-white dark:bg-neutral-800 lg:min-w-[100px] min-w-full min-h-[100px] lg:min-h-full">
-      Navbar
+    <div className=" lg:relative flex flex-row lg:flex-col justify-between  absolute bottom-0 dark:text-white bg-white dark:bg-neutral-800 lg:min-w-[100px] min-w-full min-h-[100px] lg:min-h-full">
+      <div className=" flex items-center lg:p-5 p-2 justify-center" >
+        <img className="max-h-[40px] lg:max-h-[60px]" src="\images\logo.png" alt="" />
+      </div>
+      <div className="menu lg:h-full ">
+          <div className=" flex lg:flex-col flex-row items-center border h-full w-full px-2 lg:px-0 gap-2 lg:gap-5">
+            <MenuButton href={'/'} hrefText = {<IoMdHome />} />
+            <MenuButton href={'/report'} hrefText = {<BsFileEarmarkBarGraph />} />
+            <MenuButton href={'/task'} hrefText = {<BiTask />} />
+            <MenuButton href={'/account'} hrefText = {<CiWallet />} />
+            <MenuButton href={'/stock'} hrefText = {<BsBagCheck />} />
+          </div>
+      </div>
+      <div className="logoutWrapper border flex items-center px-2">
+            <LogoutBtn />
+      </div>
     </div>
   )
 }
